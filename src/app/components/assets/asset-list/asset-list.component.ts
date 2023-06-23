3f748d4876808df5ce080776b67636683ea65018
gettaz <gettazweig@gmail.com>
2023-06-24 01:00:14 +0300

    builds

--- src/app/components/assets/asset-list/asset-list.component.ts
+++ src/app/components/assets/asset-list/asset-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Asset } from '../../../models/Asset';
+import { DbService } from '../../../services/db.service';
+import { Observable } from 'rxjs';
+import { DocumentReference } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-asset-list',
   templateUrl: './asset-list.component.html',
   styleUrls: ['./asset-list.component.css']
 })
-export class AssetListComponent {
+
+export class AssetListComponent implements OnInit {
   assets : Asset[] = [
     {
       name: 'Asset 1',
@@ -21,15 +25,37 @@ export class AssetListComponent {
       broker: 'Broker 1',
       category: 'Category 1'
     },
-    // ... more asset data
   ];
-
+  
   showForm = false;
-  newAsset: any = {};
+  newAsset: Asset = new Asset(
+    '', '', 0, 0, 0, 0, 0, 0, 0, '', ''
+  );
+
+  constructor(private dbService: DbService) {}
+
+  ngOnInit(): void {
+    this.loadAssets();
+  }
+
+  loadAssets(): void {
+    this.dbService.getAssets().subscribe((assets: Asset[]) => {
+      this.assets = assets;
+    });
+  }
 
   onAssetAdded(): void {
-    this.assets.push(this.newAsset);
-    this.closeModal();
+    //TODO: add userid
+    this.dbService.addAsset("1" ,this.newAsset).then(
+      (documentRef: DocumentReference<Asset>) => {
+        // Process the returned document reference here
+        console.log(documentRef);
+      },
+      (error) => {
+        // Handle any errors that occurred during the addition
+        console.error('Error adding asset:', error);
+      }
+    );
   }
 
   openModal(): void {
@@ -38,6 +64,8 @@ export class AssetListComponent {
 
   closeModal(): void {
     this.showForm = false;
-    this.newAsset = {}; // Reset the new asset object
+    this.newAsset = new Asset(
+      '', '', 0, 0, 0, 0, 0, 0, 0, '', ''
+    ); // Reset the new asset object
   }
 }
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,25 +1,19 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { auth } from 'firebase/app';
+import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
+import { Asset } from '../models/Asset';
+import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class DbService {
-    constructor(
-      private firestore: AngularFirestore,
-    ) {}
-  
-    // Method for saving user data
-    saveUserData(userData: any): Promise<void> {
-        const userId = auth().currentUser.uid; // Update the reference to auth
-        return this.firestore.doc(`users/${userId}`).set(userData); // Update the reference to firestore
-      }
-  
-    // Method for retrieving user data
-    getUserData(): Promise<any> {
-        const userId = auth().currentUser.uid; // Update the reference to auth
-        return this.firestore.doc(`users/${userId}`).valueChanges().toPromise(); // Update the reference to firestore
-      }
+  providedIn: 'root'
+})
+export class DbService {
+  constructor(private firestore: AngularFirestore) {}
+
+  getAssets(): Observable<Asset[]> {
+    return this.firestore.collection<Asset>('assets').valueChanges();
   }
-  
+
+  addAsset(userId: string, asset: Asset): Promise<DocumentReference<any>> {
+    return this.firestore.collection('users').doc(userId).collection('assets').add(asset);
+  }
+}
